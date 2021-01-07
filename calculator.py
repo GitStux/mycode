06008bf5f7ff6e4a7f79e8b1b434df895e977c3e
Matt Janousek <mjanousek23@gmail.com>
2021-01-07 05:19:17 +0000

    added docstring

--- calculator.py
+++ calculator.py
@@ -5,39 +5,38 @@ Author: Matt Janousek
 This program calculates user input.
 """
 
+def calculator(num1, num2):
+    """Calculator function that takes user input from main() and computes"""
+    while True:
+        operators = ["+", "-", "/", "*"]
+        user_operator = input(f"Please select one of the following operators {operators}: ")
 
-def calculator():
 
+        if user_operator == "+":
+            print(num1 + num2)
+            break
+        elif user_operator == "*":
+            print(num1 * num2)
+            break
+        elif user_operator == "/":
+            print(num1 / num2)
+            break
+        elif user_operator == "-":
+            print(num1 - num2)
+            break
+        else:
+            print("you need to select one of the pictured operators!")
+
+def main():
+    """Main method that asks for user input, then passes that information to the calculator()"""
+    while True:
         try:
             num1 = float(input("Select a number: "))
-            num2 = float(input(f"Select another number to do math stuff to {num1}: "))
-            operators = ["+", "-", "/", "*"]
-
-            while True:
-
-                user_operator = input(f"Please select one of the following operators {operators}: ")
-
-                if user_operator == "+":
-                    print (str(num1 + num2))
-                    break
-                elif user_operator == "*":
-                    print(str(num1 * num2))
-                    break
-
-                elif user_operator == "/":
-                    print(str(num1 / num2))
-                    break
-
-                elif user_operator == "-":
-                    print(str(num1 - num2))
-                    break
-
-
-                elif user_operator not in operators:
-                    print("you need to select one of the picture operators!")
+            num2 = float(input("Select another number: "))
+            calculator(num1, num2)
+            break
         except:
-            print("need numbers")
-            calculator()
+            print("Please only input numbers. ")
 
 if __name__ == "__main__":
-    calculator()
+    main()
